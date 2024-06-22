450396c18fd2a41acc598812739d8350960b6ae7
NamKunCon <namnguyen212003@gmail.com>
2024-06-22 10:26:42 +0700

    api for studentprofile

--- src/lib/api/user-api.ts
+++ src/lib/api/user-api.ts
@@ -1,26 +1,39 @@
 import { axiosClient } from "./config/axios-client";
 
 export const handleApiError = (error: any) => {
-    try {
-        const errorMessage = error.response?.data || 'An unexpected error occurred.';
-        const data = null;
-        return { error: errorMessage, data };
-    } catch (err) {
-        throw new Error('An unexpected error occurred.');
-    }
+  try {
+    const errorMessage = error.response?.data || 'An unexpected error occurred.';
+    const data = null;
+    return { error: errorMessage, data };
+  } catch (err) {
+    throw new Error('An unexpected error occurred.');
+  }
 };
 
 export const getAllUsers = async () => {
   const response: any = { error: null, data: null, success: false }
-    try {
-        const { data } = await axiosClient.get("api/v1/users");
-        if (data) {
-            response.data = data;
-            response.success = true;
-        }
-      return response;
-    } catch (error) {
-      return handleApiError(error);
+  try {
+    const { data } = await axiosClient.get("api/v1/users");
+    if (data) {
+      response.data = data;
+      response.success = true;
     }
+    return response;
+  } catch (error) {
+    return handleApiError(error);
   }
-  
+}
+
+export const getUserById = async (userId: number) => {
+  const response: any = { error: null, data: null, success: false }
+  try {
+    const { data } = await axiosClient.get(`/api/v1/users/idTmp?idTmp=${userId}`);
+    if (data) {
+      response.data = data;
+      response.success = true;
+    }
+    return response;
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
