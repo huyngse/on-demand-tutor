04a9d5015db52388c5c3b3ffa863368f8dd50bf8
huyngse <ngogiahuy775309@gmail.com>
2024-06-25 17:59:47 +0700

    Add Tutor Dashboard

--- src/lib/api/tutor-api.ts
+++ src/lib/api/tutor-api.ts
@@ -1,15 +1,15 @@
 // import { axiosClient } from './config/axios-client';
 import { tutorData } from "@/data/tutor";
+import { axiosClient } from "./config/axios-client";
 export const handleApiError = (error: any) => {
   try {
-    const errorMessage = error.response?.data || 'An unexpected error occurred.';
+    const errorMessage = error.Errors?.ErrorMessage || 'An unexpected error occurred.';
     const data = null;
     return { error: errorMessage, data };
   } catch (err) {
     throw new Error('An unexpected error occurred.');
   }
 };
-
 export const getAllTutors = async () => {
   try {
     return { error: null, data: tutorData, success: true };
@@ -17,20 +17,32 @@ export const getAllTutors = async () => {
     return handleApiError(error);
   }
 }
-
-export const getTutorById = async (id: number) => {
-  try {
-    const tutor = tutorData.find(tutor => tutor.id == id);
-    return { error: null, data: tutor, success: true };
-  } catch (error) {
-    return handleApiError(error);
-  }
+export type TutorStatisticType = {
+  noOfCreatedClasses: number;
+  noOfActiveClasses: number;
+  noOfBooking: number;
+  noOfRequest: number;
+  recentBooking: any[];
 }
-
-export const getTutorByAccountId = async (accountId: number) => {
+export const getTutorStatistic = async () => {
+  const response: any = { error: null, data: null, success: false }
+  const responseData: TutorStatisticType = {
+    noOfCreatedClasses: 0,
+    noOfActiveClasses: 0,
+    noOfBooking: 0,
+    noOfRequest: 0,
+    recentBooking: []
+  }
   try {
-    const tutor = tutorData.find(tutor => tutor.accountId == accountId);
-    return { error: null, data: tutor, success: true };
+    const classesResult = await axiosClient.get("/api/v1/classs/classes");
+    if (classesResult && classesResult.data.Errors) {
+      response.error = classesResult.data.Errors
+    } else {
+      responseData.noOfCreatedClasses = classesResult.data.length;
+      response.data = responseData;
+      response.success = true;
+    }
+    return response;
   } catch (error) {
     return handleApiError(error);
   }
--- src/pages/tutor/dashboard/TutorDashboardPage.tsx
+++ src/pages/tutor/dashboard/TutorDashboardPage.tsx
@@ -1,6 +1,47 @@
+
 const TutorDashboardPage = () => {
   return (
-    <div>TutorDashboardPage</div>
+    <div className="grid grid-cols-12 gap-5">
+      <div className="col-span-4">
+        <div className="p-3 rounded-lg drop-shadow bg-white flex flex-col items-center justify-center h-[170px]">
+          <p className="text-xl mb-3">Lớp đã tạo</p>
+          <p className="text-5xl">
+            {0}
+          </p>
+        </div>
+      </div>
+      <div className="col-span-4">
+        <div className="p-3 rounded-lg drop-shadow bg-white flex flex-col items-center justify-center h-[170px]">
+          <p className="text-xl mb-3">Lớp đang dạy</p>
+          <p className="text-5xl">
+            {0}
+          </p>
+        </div>
+      </div>
+      <div className="col-span-4">
+        <div className="p-3 rounded-lg drop-shadow bg-white flex flex-col items-center justify-center h-[170px]">
+          <p className="text-xl mb-3">Số Lượt đặt lịch dạy</p>
+          <p className="text-5xl">
+            {0}
+          </p>
+        </div>
+      </div>
+      <div className="col-span-8">
+        <div className="p-3 rounded-lg drop-shadow bg-white flex flex-col items-center justify-center h-[170px]">
+          <p className="text-xl mb-3">Lượt đặt lịch học gần đây</p>
+          <p className="text-5xl">
+          </p>
+        </div>
+      </div>
+      <div className="col-span-4">
+        <div className="p-3 rounded-lg drop-shadow bg-white flex flex-col items-center justify-center h-[170px]">
+          <p className="text-xl mb-3">Số lời mời dạy</p>
+          <p className="text-5xl">
+            {0}
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
