9a9ca69ffc8fa602fd1e077d73ad763092b9a048
huyngse <ngogiahuy775309@gmail.com>
2024-07-04 20:50:14 +0700

    Add Search Tutor API

--- src/lib/api/user-api.ts
+++ src/lib/api/user-api.ts
@@ -56,6 +56,34 @@ export const getTutorbyId = async (userId: number) => {
   }
 }
 
+export const searchTutor = async (searchQuery: string) => {
+  const response: any = {
+    error: null,
+    data: null,
+    success: false,
+    currentPage: 0,
+    totalCount: 0,
+    totalPages: 0,
+  }
+  if (searchQuery.length > 0) {
+    searchQuery = "?" + searchQuery;
+  }
+
+  try {
+    const { data, headers } = await axiosClient.get(`api/v1/users/tutor/search${searchQuery}`);
+    if (data) {
+      response.currentPage = parseInt(headers["x-current-page"]);
+      response.totalCount = parseInt(headers["x-total-count"]);
+      response.totalPages = parseInt(headers["x-total-pages"]);
+      response.data = data;
+      response.success = true;
+    }
+    return response;
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
+
 export const updateUserProfileImage = async (userId: number, imageUrl: string) => {
   const response: any = { error: null, data: null, success: false }
   try {
