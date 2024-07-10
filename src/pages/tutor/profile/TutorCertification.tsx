0496fc33f429f97e25569925fe503e8eafc22d57
NamKunCon <namnguyen212003@gmail.com>
2024-07-10 10:26:14 +0700

    update tutordegrees page

--- src/pages/tutor/profile/TutorCertification.tsx
+++ src/pages/tutor/profile/TutorCertification.tsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { getTutorDegrees } from '@/lib/api/tutor-api'; 
 import AchievementCard from '@/pages/home/tutor-detail/AchievementCard';
+import { useNavigate } from "react-router-dom";
+import { Button, Dropdown, MenuProps } from 'antd';
+import { EllipsisVertical, ImageUp, Trash2 } from "lucide-react";
 
 
 const TutorCertification = ({ tutorId }: any ) => {
   const [degrees, setDegrees] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const handleDelete = () => {
+}
+  const items: MenuProps['items'] = [
+    {
+      label: "Cập nhật ảnh bằng cấp",
+      key: 'update-pfp',
+      icon: <ImageUp width={15} />,
+    },
+    {
+      label: <div className="text-red-500 opacity-100">Gỡ ảnh bằng cấp</div>,
+      key: '3',
+      onClick: handleDelete,
+      icon: <Trash2 width={15} color="red" className="opacity-100"/>,
+      disabled: false,
+  }
+  ]
 
   useEffect(() => {
     const fetchDegrees = async () => {
@@ -31,7 +51,10 @@ const TutorCertification = ({ tutorId }: any ) => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">Tutor Degrees</h1>
+      <h1 className="text-3xl font-bold mb-2">Tutor Degrees</h1>
+          <Dropdown menu={{ items }} trigger={['click']}>
+            <Button type="default" shape="circle" icon={<EllipsisVertical width={15} />} className="me-3" />
+          </Dropdown>
       <div className="grid grid-cols-1 gap-4">
         {degrees.map((degree) => (
           <AchievementCard key={degree} data={degree} />
