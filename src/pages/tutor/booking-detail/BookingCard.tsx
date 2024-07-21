ec88c26a7ff133ce077278fbbdd7d724a348da0c
huyngse <ngogiahuy775309@gmail.com>
2024-07-21 20:38:07 +0700

    Add Tutor - Booking detail page

---
+++ src/pages/tutor/booking-detail/BookingCard.tsx
@@ -0,0 +1,387 @@
+import { UpdateBookingDto, changeBookingStatus, getTutorBooking, updateBooking } from "@/lib/api/booking-api";
+import { checkDateInterference, formatDate, getTimeString } from "@/utils/dateUtil"
+import { Button, DatePicker, Form, FormProps, Input, Modal, Popconfirm, Radio } from "antd"
+import TextArea from "antd/es/input/TextArea";
+import { useEffect, useState } from "react";
+import type { PopconfirmProps } from 'antd';
+import dayjs, { Dayjs } from "dayjs";
+import DefaultPfp from "@/assets/images/default_profile_picture.jpg"
+import { toast } from "react-toastify";
+import CancelBookingButton from "../class-detail/CancelBookingButton";;
+import { useAppSelector } from "@/hooks/useRedux";
+
+type BookingCardProps = {
+    classMethod: string;
+    bookingData: any;
+    rerender: () => void;
+}
+
+type FieldType = {
+    description: string,
+    address: string,
+    duration: Dayjs[],
+};
+
+const { RangePicker } = DatePicker;
+const BookingCard = ({ classMethod, bookingData, rerender }: BookingCardProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [form] = Form.useForm();
+    const loggedUser = useAppSelector(state => state.user.loggedUser);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleDenyBooking = async () => {
+        const denyResult = await changeBookingStatus(bookingData.bookingId, "Denied");
+        if (denyResult.error) {
+            toast.error("Cập nhật thông tin thất bại");
+        } else {
+            toast.success("Cập nhật thông tin thành công");
+            setTimeout(() => {
+                rerender();
+            }, 1000);
+        }
+    }
+
+    const handleStartTeaching = async () => {
+        const denyResult = await changeBookingStatus(bookingData.bookingId, "Started");
+        if (denyResult.error) {
+            toast.error("Cập nhật thông tin thất bại");
+        } else {
+            toast.success("Cập nhật thông tin thành công");
+            setTimeout(() => {
+                rerender();
+            }, 1000);
+        }
+    }
+
+    const handleEndTeaching = async () => {
+        const denyResult = await changeBookingStatus(bookingData.bookingId, "Ended");
+        if (denyResult.error) {
+            toast.error("Cập nhật thông tin thất bại");
+        } else {
+            toast.success("Cập nhật thông tin thành công");
+            setTimeout(() => {
+                rerender();
+            }, 1000);
+        }
+    }
+
+    // const handleCancelBooking = async () => {
+    //     const cancelResult = await changeBookingStatus(bookingData.bookingId, "Cancelled_by_tutor");
+    //     if (cancelResult.error) {
+    //         toast.error("Cập nhật thông tin thất bại");
+    //     } else {
+    //         toast.success("Cập nhật thông tin thành công");
+    //         setTimeout(() => {
+    //             rerender();
+    //         }, 1000);
+    //     }
+    // }
+
+    const denyBooking_confirm: PopconfirmProps['onConfirm'] = () => {
+        handleDenyBooking();
+    };
+
+    const denyBooking_cancel: PopconfirmProps['onCancel'] = () => {
+    };
+
+    // const cancelBooking_confirm: PopconfirmProps['onConfirm'] = () => {
+    //     handleCancelBooking();
+    // };
+
+    // const cancelBooking_cancel: PopconfirmProps['onCancel'] = () => {
+    // };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        const bookingResult = await getTutorBooking(loggedUser.userId);
+        if (bookingResult.error) {
+            toast.error("Lấy thông tin đặt lịch thất bại");
+            return;
+        }
+        const bookings = bookingResult.data;
+        const overlapBooking: any[] = [];
+        bookings.forEach((booking: any) => {
+            var condition_1 = (booking.status == "Accepted" || booking.status == "Started");
+            var condition_2 = booking.schedule.dateOfWeek == bookingData.schedule.dateOfWeek;
+            var startTimeDateA = new Date(booking.schedule.startTime);
+            startTimeDateA.setDate(1);
+            startTimeDateA.setMonth(0);
+            startTimeDateA.setFullYear(2000);
+            var startTimeDateB = new Date(bookingData.schedule.startTime);
+            startTimeDateB.setDate(1);
+            startTimeDateB.setMonth(0);
+            startTimeDateB.setFullYear(2000);
+            var endTimeDateA = new Date(booking.schedule.endTime);
+            endTimeDateA.setDate(1);
+            endTimeDateA.setMonth(0);
+            endTimeDateA.setFullYear(2000);
+            var endTimeDateB = new Date(bookingData.schedule.endTime);
+            endTimeDateB.setDate(1);
+            endTimeDateB.setMonth(0);
+            endTimeDateB.setFullYear(2000);
+            var condition_3 = checkDateInterference(
+                startTimeDateA,
+                endTimeDateA,
+                startTimeDateB,
+                endTimeDateB
+            )
+            var condition_4 = checkDateInterference(
+                new Date(booking.startDate),
+                new Date(booking.endDate),
+                new Date(values.duration[0].toISOString()),
+                new Date(values.duration[1].toISOString())
+            )
+            if (condition_1 && condition_2 && condition_3 && condition_4) {
+                overlapBooking.push(booking);
+            }
+        })
+        if (overlapBooking.length > 0) {
+            var alertString = "Không thể chấp nhận lịch học do thời gian dạy học lớp này bị trùng với thời gian của lớp khác bạn đang dạy: ";
+            overlapBooking.forEach((booking: any) => {
+                alertString += `
+            Lớp ${booking.class.className}:
+            Học sinh:  ${booking.student.fullName};
+            Bắt đầu từ ngày ${formatDate(new Date(booking.startDate))} đến ${formatDate(new Date(booking.startDate))};
+            ${booking.schedule.dateOfWeek == 0 ? "Thứ hai, tư, sáu" : "Thứ ba, năm, bảy"};
+            Thời gian từ ${getTimeString(new Date(booking.schedule.startTime))} đến ${getTimeString(new Date(booking.schedule.endTime))}.
+            `
+            })
+            alert(alertString);
+            return;
+        }
+        const requestBody: UpdateBookingDto = {
+            description: values.description,
+            address: classMethod == "In-person" ? values.address : "",
+            startDate: values.duration[0].toISOString(),
+            endDate: values.duration[1].toISOString(),
+            status: "Accepted"
+        }
+        const acceptResult = await updateBooking(bookingData.bookingId, requestBody);
+        if (acceptResult.error) {
+            toast.error("Cập nhật thông tin thất bại");
+        } else {
+            toast.success("Cập nhật thông tin thành công");
+            setTimeout(() => {
+                rerender();
+            }, 1000);
+        }
+        handleOk();
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    var statusEl;
+    switch (bookingData.status) {
+        case "Pending": {
+            statusEl = <span className="text-blue-500">Đang chờ xác nhận</span>
+            break;
+        }
+        case "Denied": {
+            statusEl = <span className="text-red-500">Đã từ chối</span>
+            break;
+        }
+        case "Accepted": {
+            statusEl = <span className="text-green-500">Đã chấp nhận</span>
+            break;
+        }
+        case "Cancelled": {
+            statusEl = <span className="text-gray-500">Đã hủy</span>
+            break;
+        }
+        case "Cancelled_by_tutor": {
+            statusEl = <span className="text-gray-500">Đã hủy bởi gia sư</span>
+            break;
+        }
+        case "Cancelled_by_student": {
+            statusEl = <span className="text-gray-500">Đã hủy bởi học sinh</span>
+            break;
+        }
+        case "Started": {
+            statusEl = <span className="text-blue-500">Đang dạy</span>
+            break;
+        }
+        case "Ended": {
+            statusEl = <span className="text-orange-500">Đã kết thúc</span>
+            break;
+        }
+    }
+
+    useEffect(() => {
+        form.setFieldValue("description", bookingData.description);
+        form.setFieldValue("address", bookingData.address);
+        form.setFieldValue("status", bookingData.status);
+        form.setFieldValue("duration", [dayjs(bookingData.startDate), dayjs(bookingData.endDate)]);
+    }, [])
+
+    return (
+        <div className="bg-white drop-shadow p-3 rounded-lg">
+            <div className="flex flex-col">
+                {
+                    bookingData.startDate && bookingData.endDate && (
+                        <>
+                            <p>
+                                <span className="font-semibold">Ngày bắt đầu học: </span>{formatDate(new Date(bookingData.startDate))}
+                            </p>
+                            <p>
+                                <span className="font-semibold">Ngày kết thúc học: </span>{formatDate(new Date(bookingData.endDate))}
+                            </p>
+                        </>
+                    )
+                }
+                {
+                    classMethod == "In-person" && (
+                        <p>
+                            <span className="font-semibold">Địa chỉ dạy: </span>{bookingData.address}
+                        </p>
+                    )
+                }
+                <p>
+                    <span className="font-semibold">Ghi chú: </span>{bookingData.description}
+                </p>
+                <p>
+                    <span className="font-semibold">Ngày đặt: </span>{formatDate(new Date(bookingData.createDate))}
+                </p>
+                <p className="font-semibold">
+                    Trạng thái: <span className="">{statusEl}</span>
+                </p>
+                {
+                    (
+                        bookingData.status == "Cancelled_by_student" ||
+                        bookingData.status == "Cancelled_by_tutor" ||
+                        bookingData.status == "Cancelled") &&
+                    (bookingData.cancellationReason?.length > 0) && (
+                        <p>
+                            <span className="font-semibold">Lý do hủy: </span>{bookingData.cancellationReason}
+                        </p>
+                    )
+                }
+                <div className="flex gap-2 justify-end flex-1 items-end">
+                    {
+                        bookingData.status == "Pending" && (
+                            <>
+                                <Popconfirm
+                                    title="Từ chối đơn đặt"
+                                    description="Xác nhận từ chối đơn đặt lịch dạy này?"
+                                    onConfirm={denyBooking_confirm}
+                                    onCancel={denyBooking_cancel}
+                                    okText="Từ chối"
+                                    cancelText="Hủy"
+                                >
+                                    <Button danger>Từ chối đơn đặt</Button>
+                                </Popconfirm>
+                                <Button type="primary" onClick={showModal}>Chấp nhận đơn đặt</Button>
+                            </>
+                        )
+                    }
+                    {
+                        bookingData.status == "Accepted" && (
+                            <>
+                                <CancelBookingButton
+                                    bookingId={bookingData.bookingId}
+                                    rerender={rerender}
+                                />
+                                <Button
+                                    type="primary"
+                                    onClick={handleStartTeaching}
+                                >
+                                    Bắt đầu dạy
+                                </Button>
+                            </>
+                        )
+                    }
+                    {
+                        bookingData.status == "Started" && (
+                            <>
+                                <CancelBookingButton
+                                    bookingId={bookingData.bookingId}
+                                    rerender={rerender}
+                                />
+                                <Button
+                                    type="primary"
+                                    onClick={handleEndTeaching}
+                                >Kết thúc dạy
+                                </Button>
+                            </>
+                        )
+                    }
+                </div>
+            </div>
+            <Modal
+                title="Xác nhận đơn đặt dạy học"
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={() => (
+                    <>
+                    </>
+                )}
+            >
+                <Form
+                    name="accept_booking"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                    form={form}
+                    layout="vertical"
+                >
+                    <Form.Item
+                        label="Ghi chú"
+                        name="description"
+                        rules={[{ required: true, message: 'Vui lòng nhập ghi chú' }]}
+                    >
+                        <TextArea placeholder="Ghi chú" disabled />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Địa chỉ học"
+                        name="address"
+                        rules={[{ required: classMethod == "In-person", message: 'Vui lòng nhập chi tiết địa chỉ dạy học' }]}
+                        className={`${classMethod == "Online" && "hidden"}`}
+                    >
+                        <Input placeholder="Địa chỉ học" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Ngày học"
+                        name="duration"
+                        rules={[{ required: true, message: 'Vui lòng nhập chi tiết địa chỉ dạy học' }]}
+                    >
+                        <RangePicker format={"DD/MM/YYYY"} minDate={dayjs()} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Phương thức thanh toán"
+                        rules={[{ required: true, message: 'Vui lòng chọn phương thức thanh toán!' }]}
+                    >
+                        <Radio.Group disabled defaultValue="cash">
+                            <Radio value="cash"> Thanh toán trực tiếp </Radio>
+                            <Radio value="vnpay" disabled> Thanh toán VNPay </Radio>
+                        </Radio.Group>
+                    </Form.Item>
+                    <div className="flex gap-2 justify-center">
+                        <Button type="default" htmlType="button" onClick={handleCancel}>
+                            Hủy
+                        </Button>
+                        <Form.Item className="m-0">
+                            <Button type="primary" htmlType="submit">
+                                Xác nhận
+                            </Button>
+                        </Form.Item>
+                    </div>
+                </Form>
+            </Modal>
+        </div >
+    )
+}
+
+export default BookingCard
--- src/pages/tutor/booking-detail/TutorBookingDetailPage.tsx
+++ src/pages/tutor/booking-detail/TutorBookingDetailPage.tsx
@@ -1,7 +1,151 @@
+import BackButton from "@/components/BackButton";
+import TiptapView from "@/components/tiptap/TiptapView";
+import useRerender from "@/hooks/useRerender";
+import { getBookingDetailbyId } from "@/lib/api/booking-api";
+import { formatDate, getTimeString } from "@/utils/dateUtil";
+import { formatNumberWithCommas } from "@/utils/numberUtil";
+import { Loader } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import DefaultPfp from "@/assets/images/default_profile_picture.jpg"
+import BookingCard from "./BookingCard";
+
 const TutorBookingDetailPage = () => {
-  
+    const [bookingDetail, setBookingDetail] = useState<any>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [pfp, setPfp] = useState<string>("");
+    const { bookingId } = useParams();
+    const { renderKey, rerender } = useRerender();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            if (bookingId) {
+                const bookingDetailResult = await getBookingDetailbyId(parseInt(bookingId));
+                if (bookingDetailResult.error) {
+                    toast.error("Lấy thông tin thất bại", {
+                        toastId: 'error_bookingDetail',
+                    });
+                } else {
+                    setBookingDetail(bookingDetailResult.data);
+                    setPfp(bookingDetailResult.data.student.profileImage);
+                }
+            }
+            setIsLoading(false);
+        }
+        fetchData();
+    }, [renderKey]);
+    if (isLoading) return <Loader />
+    if (!bookingDetail) return;
     return (
-        <div>TutorBookingDetailPage</div>
+        <div>
+            <div className="mx-5 pt-3">
+                <BackButton title="Quay về" iconWidth={15} />
+            </div>
+            <div className="grid grid-cols-12">
+                <div className="col-span-3 p-3">
+                    <div className="p-3 drop-shadow bg-white">
+                        <h1 className="font-semibold text-lg mb-2">Thông tin học sinh   </h1>
+                        <div className="flex justify-center">
+                            <div className="overflow-hidden drop-shadow rounded-lg aspect-square mx-5">
+                                <img src={pfp} alt="" className="w-full h-full object-cover" onError={() => { setPfp(DefaultPfp) }} />
+                            </div>
+                        </div>
+                        <h4 className="font-bold text-center my-3 text-lg">{bookingDetail.student.fullName}</h4>
+                        <p><span className="font-semibold">Số điện thoại: </span>{bookingDetail.student.phoneNumber}</p>
+                        <p><span className="font-semibold">Email:  </span>{bookingDetail.student.emailAddress}</p>
+                        <p><span className="font-semibold">Địa chỉ: </span>{bookingDetail.student.street}, {bookingDetail.student.ward}, {bookingDetail.student.district}, {bookingDetail.student.city}</p>
+                    </div>
+                </div>
+                <div className="col-span-9 p-3">
+                    <div className="bg-white rounded-lg drop-shadow p-3 mb-5">
+                        <h2 className="font-semibold text-xl">Lớp</h2>
+                        <div className="flex justify-between">
+                            <p className="text-3xl">{bookingDetail.class.className}</p>
+                        </div>
+                        <p>Ngày tạo: {formatDate(new Date(bookingDetail.class.createdDate))}</p>
+                        <hr className="my-2" />
+                        <div className="flex gap-5 flex-wrap">
+                            <h3 className="font-semibold">
+                                Phí dạy học: &nbsp;
+                                <span className="text-blue-500">
+                                    {formatNumberWithCommas(bookingDetail.class?.classFee)}₫
+                                </span>
+                            </h3>
+                            <h3 className="font-semibold">Hình thức dạy: <span className="font-normal">{bookingDetail.class.classMethod == "In-person" ? "Dạy trực tiếp (tại nhà)" : "Dạy online"}</span></h3>
+                            <h3 className="font-semibold">Địa chỉ lớp học: <span className="font-normal">{bookingDetail.class.ward}, {bookingDetail.class.district}, {bookingDetail.class.city}</span></h3>
+                            <h3 className="font-semibold">
+                                Trạng thái lớp: &nbsp;
+                                <span className={`font-normal ${bookingDetail.class.active ? "text-green-500" : "text-red-500"}`}>
+                                    {bookingDetail.class.active ? "Đang mở" : "Đang đóng"}
+                                </span>
+                            </h3>
+                        </div>
+                        <hr className="my-2" />
+                        <h2 className="font-semibold text-xl">Thông tin lớp</h2>
+                        {
+                            bookingDetail && <TiptapView content={bookingDetail.class?.classInfo} />
+                        }
+                        <h2 className="font-semibold text-xl">Yêu cầu lớp</h2>
+                        {
+                            bookingDetail && <TiptapView content={bookingDetail.class?.classRequire} />
+                        }
+                    </div>
+                    {/*  SCHEDULE */}
+                    <section>
+                        <div className="flex justify-between  items-center my-2">
+                            <h1 className="font-bold text-xl ">Lịch dạy lớp</h1>
+                        </div>
+                        <div className="bg-white rounded-lg drop-shadow p-3">
+                            <h3 className="font-semibold">Lịch #{bookingDetail.schedule.scheduleID}</h3>
+                            <div className="flex justify-between items-center">
+                                <p className="text-xl font-semibold">
+                                    {bookingDetail.schedule.title}
+                                </p>
+                            </div>
+                            <hr className="my-2" />
+                            <h4 className="font-semibold text-lg mt-2">
+                                Ngày học
+                            </h4>
+                            <div className="flex justify-center gap-2">
+                                <p className={`px-5 font-semibold py-2 drop-shadow ${bookingDetail.schedule.dateOfWeek == 0 ? "bg-blue-500 text-white" : "bg-white"}`}>Thứ 2</p>
+                                <p className={`px-5 font-semibold py-2 drop-shadow ${bookingDetail.schedule.dateOfWeek == 1 ? "bg-blue-500 text-white" : "bg-white"}`}>Thứ 3</p>
+                                <p className={`px-5 font-semibold py-2 drop-shadow ${bookingDetail.schedule.dateOfWeek == 0 ? "bg-blue-500 text-white" : "bg-white"}`}>Thứ 4</p>
+                                <p className={`px-5 font-semibold py-2 drop-shadow ${bookingDetail.schedule.dateOfWeek == 1 ? "bg-blue-500 text-white" : "bg-white"}`}>Thứ 5</p>
+                                <p className={`px-5 font-semibold py-2 drop-shadow ${bookingDetail.schedule.dateOfWeek == 0 ? "bg-blue-500 text-white" : "bg-white"}`}>Thứ 6</p>
+                                <p className={`px-5 font-semibold py-2 drop-shadow ${bookingDetail.schedule.dateOfWeek == 1 ? "bg-blue-500 text-white" : "bg-white"}`}>Thứ 7</p>
+                                <p className={`px-5 font-semibold py-2 drop-shadow bg-white`}>Chủ nhật</p>
+                            </div>
+                            <p className="text-lg mt-2">
+                                <span className="font-semibold">Thời gian học: </span>  {getTimeString(new Date(bookingDetail.schedule.startTime))} - {getTimeString(new Date(bookingDetail.schedule.endTime))}
+                            </p>
+                            {
+                                bookingDetail.schedule.description && bookingDetail.schedule.description.length > 0 && (
+                                    <>
+                                        <h4 className="font-semibold text-lg">
+                                            Mô tả
+                                        </h4>
+                                        <p>
+                                            {bookingDetail.schedule.description}
+                                        </p>
+                                    </>
+                                )
+                            }
+                        </div>
+                    </section>
+                    {/*  BOOKING */}
+                    <section>
+                        <h5 className="font-bold my-3 text-xl">Thông tin chi tiết đơn đặt</h5>
+                        <BookingCard
+                            classMethod={bookingDetail.class.classMethod}
+                            rerender={rerender}
+                            bookingData={bookingDetail}
+                        />
+                    </section>
+                </div>
+            </div>
+        </div>
     )
 }
 
