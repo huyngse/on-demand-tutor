6700e30ba71f8988e595e0e3bc9f1480d9f9e614
huyngse <ngogiahuy775309@gmail.com>
2024-07-21 17:55:27 +0700

    Add vietnamese language for antd components

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,15 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/styles.scss';
 import AppRouter from './components/AppRouter';
-
+import { ConfigProvider } from 'antd';
+import viVN from "antd/locale/vi_VN";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppRouter />
-      <ToastContainer autoClose={3000} />
+      <ConfigProvider locale={viVN}>
+        <AppRouter />
+        <ToastContainer autoClose={3000} />
+      </ConfigProvider>
     </Provider>
   </React.StrictMode>,
 )
